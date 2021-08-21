6c1e9ec6d9168cd622d12de0b76bdc2f8f18e568
Abdelrhman shaban <51994083+Abdelrhman97@users.noreply.github.com>
2021-08-21 13:29:01 +0200

    Create 02 - Students Marks Sum.c

---
+++ C/04 - Functions/02 - Students Marks Sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender)
+{
+    int sum = 0; 
+  if ('b' == gender)
+  {
+      for (int i = 0; i < number_of_students; i += 2)
+      {
+          sum += *(marks + i); 
+      }
+  }
+  else if ('g' == gender)
+  {
+      for (int i = 1; i < number_of_students; i += 2)
+      {
+          sum += *(marks + i); 
+      }      
+  }
+  
+  return sum;
+}
+
+int main() {
