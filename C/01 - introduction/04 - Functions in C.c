c97aab8e3895c2b0719944baadaab4fc0a81439d
Abdelrhman shaban <51994083+Abdelrhman97@users.noreply.github.com>
2021-08-21 05:23:37 +0200

    Create 04 - Functions in C.c

---
+++ C/01 - introduction/04 - Functions in C.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+    
+    int ret = 0;
+    ret = (a > b ? a : b) > (c > d ? c : d)? (a > b ? a : b) : (c > d ? c : d);
+    return ret;       
+    
+     
+}
+
+
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
