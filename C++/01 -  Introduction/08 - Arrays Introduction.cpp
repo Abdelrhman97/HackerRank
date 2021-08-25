bf6391306822be2269a97a60e0cfee3d6d5efcb5
Abdelrhman shaban <51994083+Abdelrhman97@users.noreply.github.com>
2021-08-25 23:26:14 +0200

    Create 08 - Arrays Introduction.cpp

---
+++ C++/01 -  Introduction/08 - Arrays Introduction.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+        int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+    /* Write the logic to reverse the array. */
+    
+    int* ptrR = arr + (num - 1);
+    int* ptrL = arr; 
+    
+    for (i = 0; i < num / 2; i++)
+    {
+        int temp; 
+        temp = *(ptrL + i); 
+        *(ptrL + i) = *(ptrR - i);
+        *(ptrR - i) = temp; 
+    }
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;   
+    
+}
